nearest neighbors case include_costs=False fixed cost calculation. NN works fine.

Assignment1-GreedyHeuristics/algorithms.py
@@ -1,46 +1,10 @@
+from xml.etree.ElementInclude import include
 import numpy as np
 import pandas as pd
 import random
 import copy
 import matplotlib.pyplot as plt
 from utils import calculate_single_distance, calculate_min_costs, calculate_all_distances, calculate_cost, calculate_distances
-
-
-# def random_solution(starting_node, data, total_cost, chosen_nodes):
-#     """
-#     Next node is chosen randomly.
-#     """
-#     chosen_nodes.append(starting_node)
-#     while len(data) > 101:
-#         start_node = data.pop(starting_node)
-#         random_node = random.choice(list(data.keys()))
-#         end_node = data[random_node]
-#         dist = calculate_single_distance(start_node, end_node)
-#         total_cost.append(start_node['cost'])
-#         total_cost.append(dist)
-#         random_solution(random_node, data, total_cost, chosen_nodes)
-#     return sum(total_cost), chosen_nodes
-
-
-# def random_solution_iterate(data):
-#     """
-#     Start from each point and get min and max values and best set of nodes.
-#     """
-#     min_cost = np.inf
-#     max_cost = 0
-#     min_cost_nodes = []
-#     for i in range(len(data)):
-#         total_cost, chosen_nodes = random_solution(i, copy.deepcopy(data), total_cost = [], chosen_nodes = [])
-#         if total_cost < min_cost:
-#             min_cost = total_cost
-#             min_cost_nodes = chosen_nodes
-#         if total_cost > max_cost:
-#             max_cost = total_cost
-#     print(min_cost)
-#     print(max_cost)
-#     print(len(min_cost_nodes))
-#     print(min_cost_nodes)
-#     return min_cost_nodes
 
 
 def random_solution(starting_node, data, distances):
@@ -88,12 +52,14 @@ def random_solution_iterate(data):
     return min_cost, max_cost, avg_cost, min_cost_nodes
 
 
-def nearest_neighbor(starting_node, data, all_costs: np.ndarray):
+def nearest_neighbor(starting_node, data, all_costs: np.ndarray, costs_included: bool):
     all_costs[..., starting_node] = np.inf
     path = [starting_node]
-    cost = data[starting_node]['cost']
+    if costs_included:
+        cost = data[starting_node]['cost']
+    else:
+        cost = 0.
     curr_id = starting_node
-    
     for _ in range(99):
         min_index = all_costs[curr_id].argmin()
         min_value = all_costs[curr_id, min_index]
@@ -113,17 +79,18 @@ def nearest_neighbor_iterate(data, include_costs=True):
     min_cost_nodes = []
     all_costs = []
     all_distances = calculate_all_distances(data)
-    all_costs_arr = np.array([data[i]['cost'] for i in data])  # TODO add it
+    all_costs_arr = np.array([data[i]['cost'] for i in data])
     if include_costs:
         all_distances_with_costs = all_distances + all_costs_arr
     else:
         all_distances_with_costs = all_distances
-    # min_costs = calculate_min_costs(all_distances_with_costs)
-    # sorted_indices = all_distances_with_costs.argsort(axis=1)
-    # sorted_indices = [i: list(sorted_indices[i]) for i in range(sorted_indices.shape[0])]
-    for i in range(len(data)):
-        total_cost, chosen_nodes = nearest_neighbor(i, data, copy.deepcopy(all_distances_with_costs)) 
+    for i in range(1):  #TODO
+        total_cost, chosen_nodes = nearest_neighbor(i, data, copy.deepcopy(all_distances_with_costs), costs_included=include_costs)
         total_cost += all_distances[i, chosen_nodes[-1]]
+
+        if not include_costs:
+            total_cost += sum(data[i]["cost"] for i in chosen_nodes)
+
         all_costs.append(total_cost)
         if total_cost < min_cost:
             min_cost = total_cost